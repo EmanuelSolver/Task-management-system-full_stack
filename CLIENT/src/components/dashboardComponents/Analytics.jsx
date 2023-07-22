5eb784a7909f1348552db116d14d191843c4472a
Emanuel Solver <emanuelmutuiri@gmail.com>
2023-07-22 20:32:22 +0300

    added bargraph as per data in the database

--- CLIENT/src/components/dashboardComponents/Analytics.jsx
+++ CLIENT/src/components/dashboardComponents/Analytics.jsx
@@ -1,29 +1,59 @@
-import '../../stylingFiles/Analytics.css'
-import { useEffect, useRef } from 'react';
+import '../../stylingFiles/Analytics.css';
+import { apiDomain } from '../../utils/utils';
+import axios from 'axios';
+import { ContextUser } from '../../context/userContext/userContext';
+import { useContext, useState, useEffect, useRef } from 'react';
 import Chart from 'chart.js/auto';
-
+import moment from 'moment';
 
 const Analytics = () => {
   const chartRef = useRef(null);
   const chartInstanceRef = useRef(null);
+  const { user } = useContext(ContextUser);
+  const [task, setTask] = useState([]);
+  const progress = []; // Array to store progress values
+  const labels = []; // Array to store task names
+
+  const getData = async () => {
+    try {
+      const res = await axios.get(`${apiDomain}/tasks/${user.username}`, {
+        headers: { Authorization: `${user.token}` },
+      });
+      setTask(res.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
   
   useEffect(() => {
+    getData();
     const ctx = chartRef.current.getContext('2d');
 
     if (chartInstanceRef.current) {
       chartInstanceRef.current.destroy();
     }
 
+    // Populate the 'progress' and 'labels' arrays
+    task.forEach((item) => {
+      if (
+        moment(item.StartDate).isSameOrBefore(moment(), 'day') &&
+        item.Progress < 100 &&
+        moment(item.CloseDate).isAfter(moment(), 'day')
+      ) {
+        progress.push(item.Progress);
+        labels.push(item.TaskName);
+      }
+    });
+
     chartInstanceRef.current = new Chart(ctx, {
       type: 'bar',
       data: {
-        // labels: taskNameArray,
-        labels: ['supervise', 'Database Design', 'Frontend Design', 'Mobile Design', 'Backend', 'Data Analysis', 'Figma mobile design'],
+        labels: labels,
         datasets: [
           {
             label: 'Progression of Tasks',
-            data: [9, 100, 32, 45, 73, 100, 24],
-            backgroundColor: ['#58508d', '#bc5090', '#003f5c' ],
+            data: progress,
+            backgroundColor: ['#58508d', '#bc5090', '#003f5c'],
           },
         ],
       },
@@ -43,7 +73,7 @@ const Analytics = () => {
             beginAtZero: true,
             title: {
               display: true,
-              text: 'Progress in Percentage', 
+              text: 'Progress in Percentage',
               font: {
                 weight: '800',
               },
@@ -58,14 +88,16 @@ const Analytics = () => {
         chartInstanceRef.current.destroy();
       }
     };
-  }, []);
+  }, [user.token]); // Add 'user.token' as a dependency to re-render the chart when the token changes
 
-  return(
-    <div className='bar'>
-      <canvas ref={chartRef} />;
-    </div>
-  ) 
+  return (
+    <>
+      <div className='bar'>
+        <canvas ref={chartRef} />
+      </div>
+    </>
+  );
 };
 
-
 export default Analytics;
+p
